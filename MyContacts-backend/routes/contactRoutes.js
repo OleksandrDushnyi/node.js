62154d56ecc73777cdc20b05d15219c5b68ebeba
Sanyok8 <129120542+OleksandrDushnyi@users.noreply.github.com>
2024-02-10 17:56:31 +0200

    latest update

--- MyContacts-backend/routes/contactRoutes.js
+++ MyContacts-backend/routes/contactRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const router = express.Router()
-const{getContacts, createContact, getContact, updateContact, deleteContact } = require("../controllers/ContactController.js")
+const{getContacts, createContact, getContact, updateContact, deleteContact } = require("../controllers/contactController.js")
+const validateToken = require("../middleware/validateToken.js")
 
+
+router.use(validateToken)
 router.route('/').get(getContacts).post(createContact)
-
 router.route('/:id').get(getContact).put(updateContact).delete(deleteContact)
 
 
